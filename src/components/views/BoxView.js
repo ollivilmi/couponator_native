31459fa0e1c0a9964e277cecf9ac7b5ac403ea47
Pyry Kanerva <kanervapyry@gmail.com>
2018-10-04 20:29:07 +0300

    new branch for QR, fixed a bug in BoxView

--- src/components/views/BoxView.js
+++ src/components/views/BoxView.js
@@ -1,36 +1,42 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { fetchBoxes } from '../../actions/boxActions';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { fetchBoxes } from "../../actions/boxActions";
 import Box from "./components/Box";
-import { ScrollView } from 'react-native';
+import { ScrollView } from "react-native";
 
 class BoxView extends Component {
-    componentWillMount() {
-        this.props.fetchBoxes(this.props.user);
-    }
+  componentWillMount() {
+    this.props.fetchBoxes(this.props.user);
+  }
 
-    render() {
-        return  (
-            <ScrollView style={{borderColor: '#000', borderWidth: 1, padding: 15}}>
-                {
-                    this.props.boxes.map((box, index) => (
-                        <Box key={index} id={box._id} title={box.store} progress={box.progress}/>
-                    ))
-                }
-            </ScrollView>
-        )
-    }
+  render() {
+    return (
+      <ScrollView style={{ borderColor: "#000", borderWidth: 1, padding: 15 }}>
+        {this.props.boxes.map((box, index) => (
+          <Box
+            key={index}
+            id={box._id}
+            title={box.store.name}
+            progress={box.progress}
+          />
+        ))}
+      </ScrollView>
+    );
+  }
 }
 
 BoxView.propTypes = {
-    fetchBoxes: PropTypes.func.isRequired,
-    boxes: PropTypes.array.isRequired
-}
+  fetchBoxes: PropTypes.func.isRequired,
+  boxes: PropTypes.array.isRequired
+};
 
 const mapStateToProps = state => ({
-    boxes: state.container.boxes,
-    user: state.user.id
+  boxes: state.container.boxes,
+  user: state.user.id
 });
 
-export default connect(mapStateToProps, { fetchBoxes })(BoxView);
+export default connect(
+  mapStateToProps,
+  { fetchBoxes }
+)(BoxView);
